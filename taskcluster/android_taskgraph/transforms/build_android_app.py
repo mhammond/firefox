7892bec0b345653277b02fd626b311305f28df43
Nick Alexander <nalexander@mozilla.com>
2025-10-17 10:55:50 -0400

    Bug 1987757 - Part 3: Include `embedded-uniffi-bindgen` toolchain in Android tasks.
    There's no obvious way to make the toolchain conditional on being an
    artifact build, so we include it everywhere.
    
    Differential Revision: https://phabricator.services.mozilla.com/D266035

--- taskcluster/android_taskgraph/transforms/build_android_app.py
+++ taskcluster/android_taskgraph/transforms/build_android_app.py
@@ -21,6 +21,7 @@ def add_common_config(config, tasks):
         fetches = task.setdefault("fetches", {})
         fetches["toolchain"] = [
             "android-sdk-linux",
+            "linux64-embedded-uniffi-bindgen",
             "android-gradle-dependencies",
             "linux64-jdk",
         ]
