df8999ea045933bbccd0cfe297c57ee48d516110
Sandor Molnar <smolnar@mozilla.com>
2026-08-25 11:58:08 +0300

    Revert "Bug 2065678 - ./mach test-info testrun-report: detect main branch if Git repository used also in mach_commands.py. r=ahal" DONTBUILD
    This reverts commit 1ac9c360f82e17f20c6a79c02a04439ea13b6adf.

--- testing/mach_commands.py
+++ testing/mach_commands.py
@@ -1184,13 +1184,10 @@ def test_info_testrun_report(command_context, output_file):
     import testinfo
 
     ti = testinfo.TestInfoReport(verbose=True)
-    if (
-        os.environ.get("GECKO_HEAD_REPOSITORY", "") in [
-            "https://hg.mozilla.org/mozilla-central",
-            "https://hg.mozilla.org/try",
-        ]
-        or os.environ.get("GECKO_HEAD_REF", "") == "refs/heads/main"
-    ):
+    if os.environ.get("GECKO_HEAD_REPOSITORY", "") in [
+        "https://hg.mozilla.org/mozilla-central",
+        "https://hg.mozilla.org/try",
+    ]:
         # keep the original format around as data store
         runcounts = ti.get_runcounts()
         if not output_file:
